7ca8266bc4f296485bec387f4442b24defb4b7db
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-21 21:50:46 +0000

    Tentativa de fazer os switches em padrão

---
+++ SkyNetPatternSwitch/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SkyNetPatternSwitch
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                1 => "I'm fine, thank you!",
+                break;
+                2 => "Skynet",
+                break;   
+                3 => "Destroy mankind!";
+                break;
+                _ => "You got me, I'm not THAT smart!";
+                return;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}
